dfa1a0f0a7e9023f42910d65055f6216e548c828
escott2 <etriplett14@gmail.com>
2025-04-30 14:09:03 -0500

    add user login functionality with JWT authentication

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -14,13 +14,28 @@ router.post("/register", async (req, res) => {
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
-
   const newUser = new UserModel({ username, password: hashedPassword });
   await newUser.save();
-
   res.json({ message: "User Registered Successfully" });
 });
 
-// router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "User Doesn't Exist!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password is Incorrect!" });
+  }
+
+  //TODO: Replace the word "secret" with environment variable
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
